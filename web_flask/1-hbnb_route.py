c968823f91f41457b834a16e8d1b7b4e9fa7df38
rabiebn <rabie.bnmr@gmail.com>
2024-03-23 21:58:06 +0000

    1: Initial commit

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Hello route"""
+"""HBNB route"""
 
 from flask import Flask
 
@@ -11,6 +11,10 @@ app = Flask(__name__)
 def hello():
     return "Hello HBNB!"
 
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
