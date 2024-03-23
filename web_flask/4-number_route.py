6a2d51a9630f992038abd7585d06855cd2384921
rabiebn <rabie.bnmr@gmail.com>
2024-03-23 22:56:39 +0000

    4:Initial commit

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""C is fun! route"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cisfun(text):
+    txt = text.replace('_', ' ')
+    return "C {}".format(txt)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythoniscool(text='is cool'):
+    txt = text.replace('_', ' ')
+    return "Python {}".format(txt)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def isanumber(n):
+    return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
