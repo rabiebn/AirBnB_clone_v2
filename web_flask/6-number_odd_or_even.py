5923858e42d1499befd02d671df84d6ef261bfb7
rabiebn <rabie.bnmr@gmail.com>
2024-03-24 20:23:12 +0000

    6: Initial commit

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -42,7 +42,7 @@ def number_template(n):
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
-    return render_template("6-number_odd_or_even.html", n=n)
+    return render_template("6-number_odd_or_even.html", number=n)
 
 
 if __name__ == '__main__':
